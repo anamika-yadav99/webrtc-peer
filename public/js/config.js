b2b1171e454d94bb7919c058381cafe94724ca20
Anamika Yadav <anamika310.yadav@gmail.com>
2021-06-21 09:44:17 +0530

    first commit

---
+++ public/js/config.js
@@ -0,0 +1,18 @@
+turnConfig = {
+    
+
+iceServers: [
+    {   
+        urls: [ "stun:bn-turn1.xirsys.com" ]
+    },
+     {   username: "nOIEoVTNrzKMwsFbvlvAQFjVaUCAfVMDQqnLUFmvGZWHftGPFysg1HXKVE8LywR7AAAAAGDQCUBhbmFtaWtheWFkYXY5OQ==",  
+         credential: "df6f5c80-d241-11eb-a67c-0242ac140004",   
+         urls: [       
+             "turn:bn-turn1.xirsys.com:80?transport=udp",       
+             "turn:bn-turn1.xirsys.com:3478?transport=udp",       
+             "turn:bn-turn1.xirsys.com:80?transport=tcp",       
+             "turn:bn-turn1.xirsys.com:3478?transport=tcp",       
+             "turns:bn-turn1.xirsys.com:443?transport=tcp",       
+             "turns:bn-turn1.xirsys.com:5349?transport=tcp"   
+            ]}]
+        }
